c3e35e8bad3a15c0f963c700201de5688ceaeeda
JordanVScher <jordanvscher@hotmail.com>
2020-04-11 14:40:19 -0300

    Add Email model

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -1,25 +1,48 @@
 const nodemailer = require('nodemailer');
+const Email = require('../models/Email');
+
+const transporter = nodemailer.createTransport({
+  host: process.env.SMTP_HOST,
+  port: process.env.SMTP_PORT,
+  auth: {
+    user: process.env.SMTP_EMAIL,
+    pass: process.env.SMTP_PASSWORD,
+  },
+});
 
 const sendEmail = async (options) => {
-  const transporter = nodemailer.createTransport({
-    host: process.env.SMTP_HOST,
-    port: process.env.SMTP_PORT,
-    auth: {
-      user: process.env.SMTP_EMAIL,
-      pass: process.env.SMTP_PASSWORD,
-    },
-  });
-
+  const from = `${process.env.FROM_NAME}, <${process.env.FROM_EMAIL}>`; // sender address
 
   const msg = {
-    from: `${process.env.FROM_NAME}, <${process.env.FROM_EMAIL}>`, // sender address
+    from,
     to: options.email,
     subject: options.subject,
     text: options.message,
   };
 
-  const info = await transporter.sendMail(msg);
-  console.log('Message sent: %s', info.messageId);
+
+  try {
+    const info = await transporter.sendMail(msg);
+    console.log('Message sent: %s', info.messageId);
+
+    await Email.create({
+      subject: options.subject,
+      content: options.message,
+      messageID: info.messageId,
+      from,
+      sentTo: options.email,
+    }).catch((e) => { console.log('Couldnt save email', e); });
+  } catch (error) {
+    await Email.create({
+      subject: options.subject,
+      content: options.message,
+      error,
+      from,
+      sentTo: options.email,
+    }).catch((e) => { console.log('Couldnt save email', e); });
+
+    throw error;
+  }
 };
 
 
