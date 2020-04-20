388c2cd24fa487105047681b0551b357d11bdc3a
JordanVScher <jordanvscher@hotmail.com>
2020-04-19 22:12:49 -0300

    Add port text

--- server.js
+++ server.js
@@ -66,7 +66,7 @@ app.use(errorHandler);
 
 // start server
 const PORT = process.env.PORT || 5000;
-const server = app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on ${PORT}`));
+const server = app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
 
 // handle unhandled promise rejections
 process.on('unhandledRejection', (err) => {
